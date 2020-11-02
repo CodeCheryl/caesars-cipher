840507ec9a8f76ce18f356815817aa643e34862f
codeCheryl <cheryl.morgenstern@gmail.com>
2020-11-01 20:25:45 -0500

    Improved functionality
    Added clear button
    Enabled running script on enter.
    Made buttons different colors.

--- caesars-cipher.js
+++ caesars-cipher.js
@@ -1,26 +1,37 @@
 function rot13(str) {
-    str = document.getElementById("str").value.toUpperCase();
-    let code = [];
-    for (let i = 0; i < str.length; i++) {
-      code.push(str.charCodeAt(i));
-    }
-  
-    let code13 = code.map((ele) => {
-      if (ele < 65) {
-        return ele;
-      } else if (ele >= 65 && ele < 78) {
-        return ele + 13;
-      } else {
-        return ele - 13;
-      }
-    });
-  
-    let message = String.fromCharCode(...code13);
-    let log = document.createElement("p");
-    let textNode = document.createTextNode(message);
-    log.appendChild(textNode);
-    document.getElementById("decipher").appendChild(log);
-  
-    return message;
+  str = document.getElementById("str").value.toUpperCase();
+  let code = [];
+  for (let i = 0; i < str.length; i++) {
+    code.push(str.charCodeAt(i));
   }
-  
+
+  let code13 = code.map((ele) => {
+    if (ele < 65) {
+      return ele;
+    } else if (ele >= 65 && ele < 78) {
+      return ele + 13;
+    } else {
+      return ele - 13;
+    }
+  });
+
+  let message = String.fromCharCode(...code13);
+  let log = document.createElement("p");
+  let textNode = document.createTextNode(message);
+  log.appendChild(textNode);
+  document.getElementById("decipher").appendChild(log);
+
+  return message;
+}
+
+function reset() {
+  str.value = "";
+  return decipher.innerHTML = "";
+}
+
+document.getElementById("str").addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    rot13();
+  }
+});
